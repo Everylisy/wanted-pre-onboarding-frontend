3af2abac4de82534bbc1b0b1c3fd0e7c6e766213
violetwhenisawu <anashape710@gmail.com>
2023-02-02 23:49:30 +0900

    feat: axios http 요청 함수들 분리하여 작성

---
+++ src/api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const SERVER_URL = 'https://pre-onboarding-selection-task.shop/';
+async function get(endpoint, params = '') {
+  return axios.get(SERVER_URL + endpoint + params, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+    },
+  });
+}
+
+async function post(endpoint, data) {
+  const bodyData = JSON.stringify(data);
+  return axios.post(SERVER_URL + endpoint, bodyData, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+    },
+  });
+}
+
+async function put(endpoint, data) {
+  const bodyData = JSON.stringify(data);
+  return axios.put(SERVER_URL + endpoint, bodyData, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+    },
+  });
+}
+
+async function del(endpoint, params = '') {
+  return axios.delete(SERVER_URL + endpoint + '/' + params, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('userToken')}`,
+    },
+  });
+}
+
+export { get, post, put, del };
