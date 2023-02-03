fdd61d94c6622fc8f369c1286c06819793feb21b
violetwhenisawu <anashape710@gmail.com>
2023-02-03 15:35:02 +0900

    feat: Router 로그인 여부(JWT 토큰)에 따른 리다이렉트 구현

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,36 @@
-import { Routes, Route } from 'react-router-dom';
-import NotFound from './pages/NotFound';
+import { useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { AccessTokenContext } from './contexts/AccessTokenContext';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Todo from './pages/Todo';
 
 function Router() {
+  const { token } = useContext(AccessTokenContext);
+
   return (
     <Routes>
-      <Route path="/" exact element={<SignIn />}></Route>
-      <Route path="/signin" element={<SignIn />}></Route>
-      <Route path="/signup" element={<SignUp />}></Route>
-      <Route path="/todo" element={<Todo />}></Route>
-      <Route path="*" element={<NotFound />}></Route>
+      <Route
+        path="/"
+        exact
+        element={token ? <Navigate to="/todo" replace /> : <SignIn />}
+      ></Route>
+      <Route
+        path="/signin"
+        element={token ? <Navigate to="/todo" replace /> : <SignIn />}
+      ></Route>
+      <Route
+        path="/signup"
+        element={token ? <Navigate to="/todo" replace /> : <SignUp />}
+      ></Route>
+      <Route
+        path="/todo"
+        element={token ? <Todo /> : <Navigate to="/signin" replace />}
+      ></Route>
+      <Route
+        path="*"
+        element={token ? <Todo /> : <Navigate to="/signin" replace />}
+      ></Route>
     </Routes>
   );
 }
