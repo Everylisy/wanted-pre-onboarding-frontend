c1040dd73415431f9e283c57c9ed2cd344bc2e28
violetwhenisawu <anashape710@gmail.com>
2023-02-05 20:52:32 +0900

    style: createGlobalStyle로 전역 스타일, 폰트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import AccessTokenProvider from './contexts/AccessTokenProvider';
+import GlobalStyle from './GlobalStyle';
 import Router from './Router';
 
 function App() {
   return (
     <AccessTokenProvider>
+      <GlobalStyle />
       <Router />
     </AccessTokenProvider>
   );
